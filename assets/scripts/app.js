2f926b106abe4fcc6eaa72a0ddfd681a63a82458
atvars <avelavs@gmail.com>
2023-08-03 07:56:36 +0100

    added addition function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,22 @@
-alert('hello world');
+// variables
+const zerro = 0;
+let currentResult = zerro;
+
+// Functions
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+// storing add result in variable
+// const additionResult = add(2,2);
+
+//  displaying addition reslut
+currentResult = add(4, 6);
+
+// let desc = '(' + zerro + ' + 10)'
+let desc = `( ${zerro} + 10 )`;
+
+outputResult(currentResult, desc);
+
+add(5, 6);
