b7e9b52f92c5bac8c36b337abf153fe9548680e7
atvars <avelavs@gmail.com>
2023-08-04 07:29:32 +0100

    parsing string to nr

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,7 @@ let currentResult = zerro;
 
 // Functions
 function add() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + parseInt(userInput.value);
   outputResult(currentResult, ' ');
 }
 
