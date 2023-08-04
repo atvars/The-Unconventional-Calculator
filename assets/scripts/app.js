af52903e05121f4b0c64492de4aff8020dbb2475
atvars <avelavs@gmail.com>
2023-08-04 06:48:19 +0100

    added event listener and updated add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,20 +3,9 @@ const zerro = 0;
 let currentResult = zerro;
 
 // Functions
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, ' ');
 }
 
-// storing add result in variable
-// const additionResult = add(2,2);
-
-//  displaying addition reslut
-currentResult = add(4, 6);
-
-// let desc = '(' + zerro + ' + 10)'
-let desc = `( ${zerro} + 10 )`;
-
-outputResult(currentResult, desc);
-
-add(5, 6);
+addBtn.addEventListener('click', add);
