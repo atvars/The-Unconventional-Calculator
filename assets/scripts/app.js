665aa868bc6b8c6bbdf0b0a568dacf95feb0bf11
atvars <avelavs@gmail.com>
2023-08-05 07:09:01 +0100

    added subtract function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,29 @@ const zerro = 0;
 let currentResult = zerro;
 
 // Functions
+
+
+
+// addition
 function add() {
   const enteredNo = parseInt(userInput.value);
   const calcDescription = `${currentResult} + ${enteredNo}`;
   currentResult = currentResult + enteredNo;
   outputResult(currentResult, calcDescription);
 }
-
+// do the function on when addition button is pressed
 addBtn.addEventListener('click', add);
+
+// subtraction
+function subtract() {
+  const enteredNo = parseInt(userInput.value);
+  const calcDescription = `${currentResult} - ${enteredNo}`;
+  currentResult = currentResult - enteredNo;
+  outputResult(currentResult, calcDescription);
+}
+// do the function on when subtraction button is pressed
+subtractBtn.addEventListener('click', subtract);
+
+function multiply() {}
+
+function devide() {}
