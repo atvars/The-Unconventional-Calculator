3d2e12e0c4c26ffb522965cb1fbb670f5eb20913
atvars <avelavs@gmail.com>
2024-04-16 23:08:49 +0000

    started from begining

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+
+const dafaultResult = 0;
+let currentResult = dafaultResult;
+
+
+currentResult = currentResult + 10;
+
+let calculationDescription = `${dafaultResult}  + 10`;
+
+outputResult(currentResult, calculationDescription);
+
