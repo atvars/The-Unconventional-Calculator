166cc35e90309a734bea5a6392c11d748e005b92
atvars <avelavs@gmail.com>
2024-04-19 21:56:57 +0000

    created add function and checked with alert

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,12 @@
-
+// global variables
 const dafaultResult = 0;
 let currentResult = dafaultResult;
 
+// adding function
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(`the result is: ${result}`);
+}
 
 currentResult = currentResult + 10;
 
@@ -9,3 +14,4 @@ let calculationDescription = `${dafaultResult}  + 10`;
 
 outputResult(currentResult, calculationDescription);
 
+add(3,5)
